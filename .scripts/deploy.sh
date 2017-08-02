321ad320cbd704e7567f49399db6117c095a6a6c
Pauli Jokela <pauli.jokela@didstopia.com>
2017-08-02 20:45:04 +0300

    - Cleaning up .travis.yml - Creating separate extensions project with a separate NuGet package - Fixing NuGet package generation

---
+++ .scripts/deploy.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -e
+set -o pipefail
+
+BUILD_CONFIG=$1
+NUGET_API_KEY=$2
+
+if [[ "$BUILD_CONFIG" == "Release" ]]; then
+	dotnet restore
+	dotnet build --configuration ${BUILD_CONFIG}
+	dotnet pack --configuration ${BUILD_CONFIG}
+	dotnet nuget push Didstopia.FeatherJSON/bin/$BUILD_CONFIG/*.nupkg --api-key $NUGET_API_KEY --source https://www.nuget.org/api/v2/package
+    dotnet nuget push Didstopia.FeatherJSON.Extensions/bin/$BUILD_CONFIG/*.nupkg --api-key $NUGET_API_KEY --source https://www.nuget.org/api/v2/package
+fi
