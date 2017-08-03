58200248c7feff3ed9f8910ad9c8b20ece4a861e
Pauli Jokela <pauli.jokela@didstopia.com>
2017-08-03 12:42:04 +0300

    Adding automated snapshot versions that will be pushed to MyGet.

--- .scripts/deploy.sh
+++ .scripts/deploy.sh
@@ -5,11 +5,15 @@ set -o pipefail
 
 BUILD_CONFIG=$1
 NUGET_API_KEY=$2
+NUGET_SOURCE_URL=${3:-https://www.nuget.org/api/v2/package}
 
 if [[ "$BUILD_CONFIG" == "Release" ]]; then
-	dotnet restore
-	dotnet build --configuration ${BUILD_CONFIG}
-	dotnet pack --configuration ${BUILD_CONFIG}
-	dotnet nuget push Didstopia.FeatherJSON/bin/$BUILD_CONFIG/*.nupkg --api-key $NUGET_API_KEY --source https://www.nuget.org/api/v2/package
-    dotnet nuget push Didstopia.FeatherJSON.Extensions/bin/$BUILD_CONFIG/*.nupkg --api-key $NUGET_API_KEY --source https://www.nuget.org/api/v2/package
+	VERSION_SUFFIX="snapshot-$(git rev-parse --short HEAD)"
+
+	dotnet restore /p:VersionSuffix=$VERSION_SUFFIX
+	dotnet build --configuration ${BUILD_CONFIG} /p:VersionSuffix=$VERSION_SUFFIX
+	dotnet pack --configuration ${BUILD_CONFIG} --no-build --version-suffix $VERSION_SUFFIX
+
+	dotnet nuget push Didstopia.FeatherJSON/bin/$BUILD_CONFIG/*.nupkg --api-key $NUGET_API_KEY --source $NUGET_SOURCE_URL
+    dotnet nuget push Didstopia.FeatherJSON.Extensions/bin/$BUILD_CONFIG/*.nupkg --api-key $NUGET_API_KEY --source $NUGET_SOURCE_URL
 fi
